251e0146dee3bf6a14651236949dea7819f77810
MaKiNiST19 <serdar.tpbs@gmail.com>
2026-05-02 16:01:18 +0300

    adres güncellendi

--- app/(public)/iletisim/page.tsx
+++ app/(public)/iletisim/page.tsx
@@ -168,7 +168,7 @@ export default function ContactPage() {
                     <FadeInUp delay={0.2}>
                         <div className="aspect-[16/9] md:aspect-[21/9] rounded-2xl overflow-hidden">
                             <iframe
-                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3060.4!2d32.6!3d39.95!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMznCsDU3JzAwLjAiTiAzMsKwMzYnMDAuMCJF!5e0!3m2!1str!2str!4v1234567890"
+                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3057.0126920291777!2d32.6555591!3d39.985821699999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14d3375acd50c2d7%3A0x36375a505ab59f67!2sJohn%20Filippo%20Pizza%20Eryaman!5e0!3m2!1str!2str!4v1777724882121!5m2!1str!2str"
                                 width="100%"
                                 height="100%"
                                 style={{ border: 0 }}
